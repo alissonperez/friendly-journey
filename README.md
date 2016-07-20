f380ce231e76610b8cedd49c4572f29d9deb70f6
Alisson Perez <alissonperez@gmail.com>
2016-07-20 11:00:39 -0300

    Criacao/Edicao de montadoras

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Observacoes gerais
 -------------------
 
 - Utilizando a versao 3.4.3 do python.
+- Ha um bug no highligh da navegacao quando dentro de paginas de adicao (nested states).
--- front/static/front/js/app.js
+++ front/static/front/js/app.js
@@ -8,6 +8,47 @@
 	};
     });
 
+    app.directive('manageAutoMaker', function(){
+	return {
+	    restrict: 'AE',
+	    templateUrl: suJs('templates/contents/auto-makers.new.html'),
+	    controller: function($scope, AutoMaker){
+		if (! $scope.auto_maker) {
+		    $scope.auto_maker = {};
+		}
+
+		$scope.save = function() {
+		    AutoMaker.save($scope.auto_maker);
+		};
+	    }
+	};
+    });
+
+    // AutoMaker provider
+    app.provider('AutoMaker', function AutoMakerProvider(){
+	this.$get = function() {
+	    return {
+		all: function() {
+		    return [
+			{id: 1, name: 'Toyota'},
+			{id: 2, name: 'Fiat'},
+		    ]
+		},
+		save: function(automaker) {
+		    if (automaker.id) {
+			alert('Updating ' + automaker.name);
+		    }
+		    else {
+			alert('Creating ' + automaker.name);
+		    }
+		},
+		delete: function(automaker) {
+		    alert('Removing ' + automaker.name);
+		}
+	    }
+	};
+    });
+
     // Rotas
     app.config(function($stateProvider, $urlRouterProvider) {
 	// Rota default
@@ -20,7 +61,24 @@
 	    })
 	    .state('auto-makers', {
 		url: "/montadoras",
-		templateUrl: suJs('templates/contents/auto-makers.html')
+		templateUrl: suJs('templates/contents/auto-makers.html'),
+		controller: ['$scope', 'AutoMaker', function($scope, AutoMaker){
+		    $scope.list = AutoMaker.all();
+
+		    $scope.showNew = function(){
+			$scope.auto_maker = null;
+			$("#newAutoMakerModal").modal();
+		    };
+
+		    $scope.delete = function(automaker) {
+			AutoMaker.delete(automaker);
+		    };
+
+		    $scope.edit = function(automaker) {
+			$scope.auto_maker = automaker;
+			$("#newAutoMakerModal").modal();
+		    };
+		}]
 	    })
 	    .state('models', {
 		url: '/modelos',
