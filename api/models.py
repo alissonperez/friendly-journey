b8f58aded711df1d62a244af8744a6cdf1135aa8
Alisson R. Perez <alissonperez@gmail.com>
2016-07-21 14:37:06 -0300

    Adiciona model para modelo do veiculo

--- api/models.py
+++ api/models.py
@@ -3,3 +3,9 @@ from django.db import models
 
 class AutoMaker(models.Model):
     name = models.CharField(max_length=16)
+
+
+class VehicleModel(models.Model):
+    auto_maker = models.ForeignKey(AutoMaker)
+    name = models.CharField(max_length=20)
+    year = models.IntegerField()
